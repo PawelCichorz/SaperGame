9e8846ca2b896321f2f799277974d40840658f23
PawelCichorz <pawelcichorz74>
2024-11-16 18:09:35 +0100

    push to gitHub Saper Game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+'use client'
+import { useState ,useEffect} from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+  const [clickedCells, setClickedCells] = useState<string[]>([]);
+  const [points, setPoints] = useState<number>(0);
+  const [message, setMessage] = useState<string>("");
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+
+
+ 
+
+
+  const handleCellClick = (rowIndex: number, colIndex: number, cellValue: number | string) => {
+    if (isGameOver) return;
+    const cellKey = `${rowIndex}-${colIndex}`;
+    if (!clickedCells.includes(cellKey)) {
+      setClickedCells((prev) => [...prev, cellKey]);
+      if (typeof cellValue === "number") {
+        setPoints((prevPoints) => prevPoints + cellValue);
+      }
+      else if(cellValue === "bomb"){
+        setMessage("Przegrałeś");
+        setIsGameOver(true)
+      }
+    }
+  
+  };
+
+  const numberList = [
+    [12, 5, 9, 'bomb', 20],
+    ['bomb', 5, 9, 'bomb', 20],
+    [12, 7, 19, 9, 10],
+    [7, 9, 12, 'bomb', 'bomb'],
+  ];
+
+  
+
+  useEffect(() => {
+    if (points > 100) {
+      setMessage("Wygrałeś");
+      setIsGameOver(true)
+      return;
+    }
+ 
+  },[points])
+  
+  return (
+    <div className="flex justify-center items-center flex-col text-center">
+      <h2>Saper</h2>
+
+      <table className="border-collapse">
+        <tbody> 
+          {numberList.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, colIndex) => {
+                const cellKey = `${rowIndex}-${colIndex}`;
+                const isClicked = clickedCells.includes(cellKey);
+
+                return (
+                  <td
+                    key={colIndex}
+                    className={`border-2 border-black cursor-pointer ${
+                      isClicked ? 'opacity-100' : 'opacity-0'
+                    }`}
+                    onClick={() => handleCellClick(rowIndex, colIndex,cell)}
+                   
+                  >
+                    <div >{cell === 'bomb' ? '💣' : cell}</div>
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <p>Punkty: <span id="points">{points}</span></p>
+
+      <p id="message" className={`${message ? 'py-4 px-8 border-2 border-red-500 mt-8': ''}`}>{message ? message +'😊' : message}</p>
     </div>
   );
 }
